45e81632bec9b0dba4722d0eb899150c37579188
Maksym Kucherov <maximus.the0@gmail.com>
2024-05-23 09:49:04 -0400

    Expand `DeviceEnrichmentTest` class.

--- extra/modules/fiftyone-devicedetection/src/test/java/org/prebid/server/hooks/modules/fiftyone/devicedetection/v1/hooks/rawAcutionRequest/detection/DeviceEnrichmentTest.java
+++ extra/modules/fiftyone-devicedetection/src/test/java/org/prebid/server/hooks/modules/fiftyone/devicedetection/v1/hooks/rawAcutionRequest/detection/DeviceEnrichmentTest.java
@@ -3,6 +3,7 @@ package org.prebid.server.hooks.modules.fiftyone.devicedetection.v1.hooks.rawAcu
 import com.iab.openrtb.request.Device;
 import fiftyone.devicedetection.DeviceDetectionOnPremisePipelineBuilder;
 import fiftyone.devicedetection.shared.DeviceData;
+import fiftyone.pipeline.core.data.FlowData;
 import fiftyone.pipeline.core.flowelements.Pipeline;
 import org.junit.Test;
 import org.prebid.server.hooks.modules.fiftyone.devicedetection.model.boundary.CollectedEvidence;
@@ -13,6 +14,7 @@ import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -73,5 +75,88 @@ public class DeviceEnrichmentTest {
 
         // then
         assertThat(result.processingException()).isEqualTo(e);
+    }
+
+    @Test
+    public void shouldReportErrorOnProcessException() throws Exception {
+        // given
+        final Pipeline pipeline = mock(Pipeline.class);
+        final FlowData flowData = mock(FlowData.class);
+        final Exception e = new RuntimeException();
+        when(pipeline.createFlowData()).thenReturn(flowData);
+        doThrow(e).when(flowData).process();
+
+        // when
+        final BiFunction<
+                Device,
+                CollectedEvidence,
+                FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult> hook
+                = buildHook(() -> pipeline, null);
+        final FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult result = hook.apply(
+                null,
+                CollectedEvidence.builder().build());
+
+        // then
+        assertThat(result.processingException()).isEqualTo(e);
+    }
+
+    @Test
+    public void shouldReturnNullOnNullDeviceData() throws Exception {
+        // given
+        final Pipeline pipeline = mock(Pipeline.class);
+        final FlowData flowData = mock(FlowData.class);
+        final Exception e = new RuntimeException();
+        when(pipeline.createFlowData()).thenReturn(flowData);
+        when(flowData.get(DeviceData.class)).thenReturn(null);
+
+        // when
+        final BiFunction<
+                Device,
+                CollectedEvidence,
+                FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult> hook
+                = buildHook(() -> pipeline, null);
+        final FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult result = hook.apply(
+                null,
+                CollectedEvidence.builder().build());
+
+        // then
+        assertThat(result).isNull();
+    }
+
+    @Test
+    public void shouldReturnPatchResult() throws Exception {
+        // given
+        final Pipeline pipeline = mock(Pipeline.class);
+        final FlowData flowData = mock(FlowData.class);
+        when(pipeline.createFlowData()).thenReturn(flowData);
+        final Device device = Device.builder().build();
+        final DeviceData deviceData = mock(DeviceData.class);
+        when(flowData.get(DeviceData.class)).thenReturn(deviceData);
+        final Device mergedDevice = Device.builder().build();
+        final FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult preparedResult
+                = FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult.builder()
+                .enrichedDevice(mergedDevice)
+                .build();
+
+        // when
+        final boolean[] patcherCalled = { false };
+        final BiFunction<
+                Device,
+                CollectedEvidence,
+                FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult> hook
+                = buildHook(() -> pipeline,
+                (dev, devData) -> {
+                    patcherCalled[0] = true;
+                    assertThat(dev).isEqualTo(device);
+                    assertThat(devData).isEqualTo(deviceData);
+                    return preparedResult;
+                });
+        final FiftyOneDeviceDetectionRawAuctionRequestHook.EnrichmentResult result = hook.apply(
+                device,
+                CollectedEvidence.builder().build());
+
+        // then
+        assertThat(patcherCalled).containsExactly(true);
+        assertThat(result).isEqualTo(preparedResult);
     }
 }
