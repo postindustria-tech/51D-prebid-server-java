67ecd45b7243f032528160f9bcea4a253a006470
Maksym Kucherov <maximus.the0@gmail.com>
2024-06-04 14:09:52 -0400

    Update `PipelineBuilderSpawnerImpTest` to no longer rely on protected methods.

--- extra/modules/fiftyone-devicedetection/src/test/java/org/prebid/server/hooks/modules/fiftyone/devicedetection/v1/hooks/rawAcutionRequest/pipeline/PipelineBuilderSpawnerImpTest.java
+++ extra/modules/fiftyone-devicedetection/src/test/java/org/prebid/server/hooks/modules/fiftyone/devicedetection/v1/hooks/rawAcutionRequest/pipeline/PipelineBuilderSpawnerImpTest.java
@@ -1,21 +1,22 @@
 package org.prebid.server.hooks.modules.fiftyone.devicedetection.v1.hooks.rawAcutionRequest.pipeline;
 
-import fiftyone.devicedetection.DeviceDetectionOnPremisePipelineBuilder;
+import fiftyone.pipeline.core.flowelements.PipelineBuilderBase;
 import org.junit.Test;
 import org.prebid.server.hooks.modules.fiftyone.devicedetection.model.config.DataFile;
+import org.prebid.server.hooks.modules.fiftyone.devicedetection.model.config.DataFileUpdate;
+import org.prebid.server.hooks.modules.fiftyone.devicedetection.model.config.ModuleConfig;
+import org.prebid.server.hooks.modules.fiftyone.devicedetection.model.config.PerformanceConfig;
 import org.prebid.server.hooks.modules.fiftyone.devicedetection.v1.core.PipelineBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PipelineBuilderSpawnerImpTest {
-    private static PipelineBuilderSpawner<DeviceDetectionOnPremisePipelineBuilder> makeSpawner() throws Exception {
-        return new PipelineBuilder(null) {
-            @Override
-            public DeviceDetectionOnPremisePipelineBuilder makeRawBuilder(DataFile dataFile) throws Exception {
-                return super.makeRawBuilder(dataFile);
-            }
-        }
-            ::makeRawBuilder;
+    private static PipelineBuilderBase<?> spawnBuilder(DataFile dataFile) throws Exception {
+        final ModuleConfig moduleConfig = new ModuleConfig();
+        moduleConfig.setDataFile(dataFile);
+        dataFile.setUpdate(new DataFileUpdate());
+        moduleConfig.setPerformance(new PerformanceConfig());
+        return new PipelineBuilder(null).build(moduleConfig);
     }
 
     @Test
@@ -23,11 +24,9 @@ public class PipelineBuilderSpawnerImpTest {
         // given
         final DataFile dataFile = new DataFile();
         dataFile.setPath("dummy.hash");
-        final PipelineBuilderSpawner<DeviceDetectionOnPremisePipelineBuilder> builderSpawner
-                = makeSpawner();
 
         // when
-        final DeviceDetectionOnPremisePipelineBuilder pipelineBuilder = builderSpawner.spawn(dataFile);
+        final PipelineBuilderBase<?> pipelineBuilder = spawnBuilder(dataFile);
 
         // then
         assertThat(pipelineBuilder).isNotNull();
@@ -39,11 +38,9 @@ public class PipelineBuilderSpawnerImpTest {
         final DataFile dataFile = new DataFile();
         dataFile.setPath("dummy.hash");
         dataFile.setMakeTempCopy(true);
-        final PipelineBuilderSpawner<DeviceDetectionOnPremisePipelineBuilder> builderSpawner
-                = makeSpawner();
 
         // when
-        final DeviceDetectionOnPremisePipelineBuilder pipelineBuilder = builderSpawner.spawn(dataFile);
+        final PipelineBuilderBase<?> pipelineBuilder = spawnBuilder(dataFile);
 
         // then
         assertThat(pipelineBuilder).isNotNull();
